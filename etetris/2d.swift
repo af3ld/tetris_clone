69e44a6fa7b68a3f84d56d87fd36fb8ec5e6f670
af3ld <alexfeldman@lclark.edu>
2016-07-01 17:34:28 -0700

    building the board

--- etetris/2d.swift
+++ etetris/2d.swift
@@ -6,4 +6,26 @@
 //  Copyright © 2016 Alex Feldman. All rights reserved.
 //
 
-import Foundation
+class Array2d<T> {
+    
+    let cols: Int
+    let rows: Int
+    var arr: Array<T?>
+    
+    init(columns: Int, rows: Int) {
+        self.cols = columns
+        self.rows = rows
+        
+        arr = Array<T?>(count: rows * columns, repeatedValue: nil)
+    }
+    
+    subscript(column: Int, row: Int) -> T? {
+        get {
+            return arr[(row * cols) + column]
+        }
+        set(newVal) {
+            arr[(row * cols) + column] = newVal
+        }
+    }
+}
+
